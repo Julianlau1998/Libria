0e430e43c0c248dab697e34ffea833d2f800e7bf
Julian Lau <julian.lau@aol.com>
2021-08-03 20:33:55 +0200

    libria_web

---
+++ libria_web/src/router/index.js
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import Topic from '../views/Topic.vue'
+
+// import { authGuard } from "../auth/authGuard";
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/topic/:uuid',
+    name: 'Topic',
+    component: Topic,
+    // beforeEnter: authGuard
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes,
+  scrollBehavior () {
+    return { x: 0, y: 0 }
+  },
+})
+
+export default router
